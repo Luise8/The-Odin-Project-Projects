c9d3468e965f978d76391fc785e9cf2d078371d6
luis <luistenan@gmail.com>
2022-04-15 14:08:37 -0400

    Update selector to each number swith event listener to write these. Update selector to each part of screen. Update delete function. Add selectors to point, equal and operators. Add clearAll function. Add point listener to write this.

--- fundations/javascript-basics/3-project-calculator/js-file.js
+++ fundations/javascript-basics/3-project-calculator/js-file.js
@@ -1,43 +1,53 @@
-const calculatorBtn = document.querySelector("#calculator-btn");
+// Buttons
 const numbers = document.querySelectorAll(".number");
-const calculatorScreen = document.querySelector("#calculator-screen");
-const current = document.querySelector("#current");
+const clearAll = document.querySelector(".clearAll");
+const point = document.querySelector(".point");
+const equal = document.querySelector(".equal");
+const operators = document.querySelectorAll(".operators");
 
-calculatorBtn.addEventListener("click", (e) => {
-  // Check max-length of digits
-  if (current.textContent.length < 20) {
-    // Check if pressed a number
-    if (e.target.classList.contains("number")) {
-      //Check if first digit is diferent to don't allow write two zeros continue
-      if (current.textContent[0] != 0) {
-        current.textContent += e.target.textContent;
-      } else {
-        current.textContent = e.target.textContent;
+// Screen parts
+const current = document.querySelector("#current");
+const operation = document.querySelector("#operator");
+const result = document.querySelector("#result");
+
+// Numbers
+numbers.forEach((e) => {
+  if (e.textContent == "0") {
+    e.addEventListener("click", () => {
+      if (current.value != "-0") {
+        current.value += e.textContent;
       }
+    });
+  } else {
+    e.addEventListener("click", () => {
+      current.value += e.textContent;
+    });
+  }
+});
+
+// Point
+point.addEventListener("click", (e) => {
+  if (!current.value.includes(".")) {
+    if (current.value != "" && current.value != "-") {
+      current.value += e.target.textContent;
+    } else if (current.value == "") {
+      current.value = "0.";
     }
   }
+});
+
+// AC button to clear all digits in all parts of screen
+clearAll.addEventListener("click", () => {
+  result.value = "";
+  operation.value = "";
+  current.value = "0";
 });
 
 // Delete function
 const deleteOneDigit = document.querySelector(".delete");
 deleteOneDigit.addEventListener("click", () => {
-  if (current.textContent.length > 1) {
-    current.textContent = current.textContent.slice(
-      0,
-      current.textContent.length - 1
-    );
-  } else if (current.textContent.length == 1) {
-    current.textContent = 0;
+  if (current.value.length > 0) {
+    let deleteDigit = current.value[current.value.length - 1];
+    current.value = current.value.slice(0, -1);
   }
 });
-
-/* const clearAll = document.querySelector(".delete");
-clearAll.addEventListener("click", () => {});
-
-function clearAll() {
-  calculatorScreen.textContent = 0;
-}
- */
-/* operators = {
-  AC = 
-} */
